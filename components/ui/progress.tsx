b3624fdd0bf451b61e9246e03a83fe5dce6230c8
Afrizal Mufriz Fouji <63228544+aafrzl@users.noreply.github.com>
2024-05-18 17:58:09 +0700

    fix progress bar background adding opacity

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -14,7 +14,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      "relative h-4 w-full overflow-hidden rounded-full dark:bg-secondary/10 bg-secondary/30",
       className
     )}
     {...props}
